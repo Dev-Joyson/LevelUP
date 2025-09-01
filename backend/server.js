95e4acf6ae53f2e2220009089065303b96526033
Joyson <cj4dev@gmail.com>
2025-09-01 22:43:00 +0530

    small fix in cors

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"], // Frontend URLs
+    origin: ["http://localhost:3000", "http://localhost:3001","https://level-up-five.vercel.app"], // Frontend URLs
     methods: ["GET", "POST"],
     credentials: true
   }
