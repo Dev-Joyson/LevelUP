bad850a6b928bd654974d0b878ab49eb468a8582
sajith-ali <sajithalise@gmail.com>
2025-09-29 22:54:45 +0530

    TestimonialsSection_updated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ import QuickActionSection from "@/components/LandingPage/QuickActionSection";
 import DesiredDomain from "@/components/LandingPage/DesiredDomain";
 import { FeaturesSection } from "@/components/LandingPage/FeaturesSection";
 import { HeroSection } from "@/components/LandingPage/HeroSection";
-import { TestimonialsSection } from "@/components/LandingPage/TestimonialsSection";
+import TestimonialsSection from "@/components/LandingPage/TestimonialsSection";
 
 export default function Home() {
 
